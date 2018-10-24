b3852e00e53f9396ccd8a8c7267bac464f36716a
Davinais <david870620@gmail.com>
2018-10-24 17:49:42 +0800

    Make Piano Playable with AudioSynth

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+    var piano = Synth.createInstrument("piano");
+    $(".key").click(function (e) { 
+        e.preventDefault();
+        piano.play($(this).data("note"), 4, 2);
+    });
 });
