ccd930e087820c8a1a7fa14e2afa1b3e3704411d
Davinais <david870620@gmail.com>
2018-10-28 03:05:35 +0800

    Add Simple Note Indicate Functions for Piano
    Usage: noteIndicate("String: Note Names with Space", callback)
    Available Note Names: "C C# D D# E F F# G G# A A# B"
    
    Notes Indication are used to indicate users which note to play,
    and do something after users play all the indicated notes.
    
    However, this function lacks notes string checking,
    it still can be improved.

--- public/main.js
+++ public/main.js
@@ -4,7 +4,24 @@ $(document).ready(function(){
         e.preventDefault();
         piano.play($(this).data("note"), 4, 2);
     });
-	
+
+    /* Usage: noteIndicate("Note names with space: C C# D D# E F F# G G# A A# B", callback) */
+    var noteIndicate = function(n, callback) {
+        var notes = n.split(" ");
+        $("[data-note='"+notes.shift()+"']").addClass("indicate");
+        $(".keys").on("click", ".indicate", function(){
+            $(this).removeClass("indicate");
+            if(notes.length != 0) {
+                $("[data-note='"+notes.shift()+"']").addClass("indicate");
+            }
+            else {
+                callback();
+            }
+        });
+    };
+
+    //noteIndicate("C D E F G", () => { alert("Well Played!") });
+    
 	$("#left").click(function() {
 		var left = $("#team-container #slider").css( "left" );
 			$("#team-container #slider").animate({left: '-=100vh'});
