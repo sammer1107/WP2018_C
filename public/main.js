16e3529082819540894a413be84046ddf3216186
JKChun <a3535212114298@gmail.com>
2018-10-28 00:47:57 +0800

    Make the team-page left right button work.
    But it has a bug need to fix, it can be pressed even if there isn't other member.

--- public/main.js
+++ public/main.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
         e.preventDefault();
         piano.play($(this).data("note"), 4, 2);
     });
+	
+	$("#left").click(function() {
+		var left = $("#team-container #slider").css( "left" );
+			$("#team-container #slider").animate({left: '-=100vh'});
+	});
+	
+	$("#right").click(function() {
+		var left = $("#team-container #slider").css( "left" );
+			$("#team-container #slider").animate({left: '+=100vh'});
+	});
 });
