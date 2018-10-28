a00b86a218d7d9edfa9024a6515a43fe01093b04
Davinais <david870620@gmail.com>
2018-10-29 01:34:37 +0800

    Add notePlay Function
    Implement a function that can play the notes with tempo easily.
    
    Usage: notePlay("Note names with Tempo", BPM, callback)
    Symbol: Default is quarter note, '-':*2, '^':/2, '.':*1.5(once only)

--- public/main.js
+++ public/main.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     });
 
     /* Usage: noteIndicate("Note names with space: C C# D D# E F F# G G# A A# B", callback) */
-    var noteIndicate = function(n, callback) {
-        var notes = n.split(" ");
+    var noteIndicate = function(n, callback = ()=>{}) {
+        var notes = n.replace(/[\-\^\.]/g, "").split(" ");
         $("[data-note='"+notes.shift()+"']").addClass("indicate");
         $(".keys").on("click", ".indicate", function(){
             $(this).removeClass("indicate");
@@ -20,7 +20,32 @@ $(document).ready(function(){
                 callback();
             }
         });
-    };
+	};
+	
+	/* Usage: notePlay("Note names with Tempo", BPM, callback)
+	   Tempo symbol: Default is quarter note, '-':*2, '^':/2, '.':*1.5(once only) */
+	var notesPlay = function(n, bpm, callback = ()=>{}) {
+		let notes = n.split(" ");
+		let ms_per_note = 60000/bpm;
+		let play_promise = Promise.resolve();
+		notes.forEach(note => {
+			play_promise = play_promise.then(() => {
+				//Use includes("#") to decide should the 2nd pos be preserved. Ex: C#
+				let note_name = note.slice(0, 1+(note.includes("#") | 0));
+				let time_param = ms_per_note;
+				/* (split.length-1) counts the char, but here we make two
+				   nums do substraction, so (-1)-(-1)=0, no need for (-1).
+				   Here '-' means twice the time and '^' means half the time */
+				time_param *= 2**(note.split("-").length - note.split("^").length);
+				time_param *= (note.includes("."))? 1.5 : 1;
+				piano.play(note_name, 4, 2);
+				return new Promise((resolve) => {
+					setTimeout(resolve, time_param);
+				});
+			});
+		});
+		play_promise.then( callback );
+	};
 
     $.scrollify({
 		section: ".page",
@@ -31,8 +56,9 @@ $(document).ready(function(){
 	$("#piano-page .desc h2").delay(1000).fadeTo(500, 1);
 	$("#piano-page .desc p").delay(2000).fadeTo(500, 1);
 	
-    // Oh Susanna in D Major
-    noteIndicate("D E F# A A B A F# D E F# F# E D E", () => { $.scrollify.next(); });
+	// Oh Susanna in D Major
+	var song = "D^^ E^^ F#^ A^ A^. B^^ A^ F#^ D^. E^^ F#^ F#^ E^ D^ E.";
+	noteIndicate(song, () => { $.scrollify.next(); });
     
 	$(".button#left").click( slideCard );	
 	$(".button#right").click( slideCard );
