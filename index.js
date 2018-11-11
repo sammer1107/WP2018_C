5d397c231ca2642764c6bb8a84d842d5c2557ade
sammer1107 <qscesz871107@gmail.com>
2018-11-12 01:42:06 +0800

    added player connection handling
    now the server will be maintaining a list of all players which will be
    updated on connection and disconnection. The player object (at server
    side) hold the informations including the id and position. The code for
    information updating and broadcasting is not yet implemented.

--- index.js
+++ index.js
@@ -11,8 +11,37 @@ app.use('/assets', express.static(__dirname + '/assets'));
 server.listen(port);
 console.log(`listening on port ${port}`);
 
+
 var io = require('socket.io')(server, {});
+
+var players = [];
+
+var Player = function (init_x, init_y, socket_id){
+    this.x = init_x;
+    this.y = init_y;
+    this.id = socket_id;
+}
+
+function onNewPlayer(data){
+    var newPlayer = new Player(data.x, data.y, this.id);
+    console.log(`Created new player at ${data.x},${data.y}.`);
+    players.push(newPlayer);
+}
+
+function onDisconnect(){
+    console.log(players)
+    var removePlayer = players.find( p =>{
+       return p.id == this.id 
+    });
+    console.log(`player ${removePlayer.id} disconnected.`)
+    players = players.filter( p =>{
+        return p.id != this.id
+    });
+    console.log(players)
+}
 
 io.sockets.on('connection', function(socket){
     console.log(`socket ID: ${socket.id} connected.`)
+    socket.on("new_player", onNewPlayer);
+    socket.on("disconnect", onDisconnect)
 });
--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 "use strict";
 const KURO_SPEED = 300;
 
-var socket = io.connect();
-socket.on("connect", onSocketConnected); 
+var socket;
 
 function onSocketConnected(){
     console.log("Socket connected.");
+    var scene = game.scene.getScene('muziKuro');
+    createPlayer(scene, 2525, 2525);
+    socket.emit("new_player", {x:2525,y:2525})
+}
+
+function onSocketDisconnected(){
+    var scene = game.scene.getScene('muziKuro');
+    scene.player.destroy();
+}
+
+function createPlayer(scene,x,y){
+    scene.player = scene.physics.add.sprite(x,y, 'Kuro')
+    scene.player.pointerDest = null;
+    scene.player.setScale(0.3);
+    scene.player.setOrigin(0.5,1);
+    scene.player.setCollideWorldBounds(true);
+    // camera setup
+    scene.cameras.main.startFollow(scene.player);
+    scene.cameras.main.setLerp(0.1,0.1);
+    
+    scene.input.on("pointerdown", function(pointer){
+        console.log(pointer.x, pointer.y)
+        var dest = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
+        this.player.pointerDest = dest
+        this.physics.moveToObject(this.player, dest, KURO_SPEED); // This will not stop when reached destination
+    }, scene);
 }
 
 var MuziKuro = {
+    key: 'muziKuro',
     preload: function() {
         this.load.image('Kuro', '/assets/Kuro.png');
         this.load.tilemapTiledJSON('map', '/assets/map.json');
@@ -17,31 +43,23 @@ var MuziKuro = {
     },
     
     create: function(){
+        socket = io.connect();
+        socket.on("connect", onSocketConnected);
+        socket.on("disconnect", onSocketDisconnected)
+        
         // create map
 
         var map = this.make.tilemap({ key: 'map'});
         var google_tile = map.addTilesetImage('google_tile');      // name as specified in map.json
         var layer = map.createStaticLayer('map_layer_0', google_tile);
-        layer.setScale(1);
-
+        this.physics.world.setBounds(0,0,5000,5000);
+        
         // create a music note randomly
         this.music_note = this.physics.add.group();
         for (var i = 0; i < 11; i++)
         {
             this.music_note.create(2525 + Math.random() * 400, 2525 + Math.random() * 400, 'music_note');
         }
-
-        // add player
-        this.kuro = this.physics.add.sprite(2525,2525, 'Kuro');
-        this.kuro.pointerDest = null;
-        this.kuro.setScale(0.3);
-        this.kuro.setOrigin(0.5,1);
-
-        this.physics.add.overlap(this.kuro, this.music_note, collectMusicNote, null, this);
-
-        // camera setup
-        this.cameras.main.startFollow(this.kuro);
-        this.cameras.main.setDeadzone(100, 100);
         
         // controlls
         /*
@@ -50,13 +68,11 @@ var MuziKuro = {
         KEY_S = this.input.keyboard.addKey("s");
         KEY_D = this.input.keyboard.addKey("d");
         */    
-        this.input.on("pointerdown", pointerDown, this);
 
     },
     
     update: function(time, delta){     
-        var kuro = this.kuro;
-        var music_note=this.music_note;
+        var player = this.player;
         /*
         if(KEY_W.isDown){
             kuro.y -= KURO_SPEED;
@@ -75,11 +91,11 @@ var MuziKuro = {
             
         }
         */
-        if(kuro.pointerDest != null){
+        if(player.pointerDest != null){
             // stop movement when kuro reached pointer movement's destination
-            if( Phaser.Math.Distance.Between(kuro.x, kuro.y, kuro.pointerDest.x, kuro.pointerDest.y) < 3){
-                kuro.body.velocity.set(0,0);
-                kuro.pointerDest = null;
+            if( Phaser.Math.Distance.Between(player.x, player.y, player.pointerDest.x, player.pointerDest.y) < 3){
+                player.body.velocity.set(0,0);
+                player.pointerDest = null;
             }
         }
     },
@@ -91,25 +107,20 @@ function collectMusicNote (player, music_note)
     music_note.disableBody(true, true);
 }
 
-var canvas_h = window.innerHeight * window.devicePixelRatio;
-var canvas_w = window.innerWidth * window.devicePixelRatio;
+console.log(`window hieght: ${window.innerHeight}`)
+console.log(`window width: ${window.innerWidth}`)
 
 var config = {
     type: Phaser.AUTO, // renderer setting
-    width: canvas_w,
-    height: canvas_h,
-    parent: document.getElementById("game-container"),
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: "game-container",
+    render: {roundPixels: false},
     physics: {
         default: 'arcade',
     },
-    scene: [MuziKuro]
+    scene: [MuziKuro, MuziKuro],
 };
 
-var game = new Phaser.Game(config=config)
+var game = new Phaser.Game(config=config);
 
-
-function pointerDown(pointer){
-    var dest = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
-    this.kuro.pointerDest = dest
-    this.physics.moveToObject(this.kuro, dest, KURO_SPEED); // This will not stop when reached destination
-}
