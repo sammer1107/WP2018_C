3096381b1302513a1407cf263ce0cc720666d24e
sammer1107 <qscesz871107@gmail.com>
2019-01-18 00:32:46 +0800

    using css keyframe for logo animation
    also reorganized some css

--- js/client/main.js
+++ js/client/main.js
@@ -10,8 +10,8 @@ import Animation from './lib/Animation.js'
 
 var config = {
     type: Phaser.AUTO, // renderer setting
-    width: window.innerWidth*window.devicePixelRatio,
-    height: window.innerHeight*window.devicePixelRatio,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
     canvas: document.getElementById('game'),
     callbacks: {postBoot: resize},
     physics: {
@@ -24,24 +24,12 @@ var config = {
 var game;
 
 $(document).ready( function(){
-    var logo=$("#welcome .logo"),
+    var logo=$("#welcome #logo img"),
         notes=$("#welcome #notes"),
         light=$("#welcome #light");
-    
-    var logo_anim = new Animation({
-        duration: 300,
-        delay: 500,
-        update: (t)=>{
-            if(t <= 0.8){
-                logo.css("transform", `translate(-50%, -50%) scale(${2.625*t-1.6406*t*t})`);            
-            }else{
-                logo.css("transform", `translate(-50%, -50%) scale(${0.05*Math.cos(15/2*Math.PI*(t-0.8))*Math.exp(-12*(t-0.8))+1})`);
-            }
-        }
-    }).start();
         
     var light_anim = new Animation({
-        duration: 250,
+        duration: 300,
         delay: 600,
         update: (t)=>{
             light.css("clip-path", `circle(${1000*t}px at 48% 39%)`)
@@ -49,16 +37,18 @@ $(document).ready( function(){
         callback: () => light.css("clip-path", "unset") 
     }).start();
     
-    setTimeout( ()=> notes.css("transform", "scale(1)"), 800);
+    setTimeout( ()=> logo.addClass('fly-in'), 500);
+    setTimeout( ()=> notes.css("transform", "scale(1)"), 900);
     setTimeout( ()=> $("#welcome #start-button").css("top", "80%"), 1800);
     
     notes.one('transitionend', function(e) {
         notes.addClass("float-anim");
     });
     
-    $("#start-button").click(function(){
+    $("#start-button").click( function(){
         game = new Game(config);
         if(__DEBUG) window.game = game;
+        
         $("#welcome").addClass("darken");
         $("#nickname").css("top",'0px');
         $("#nickname #enter").click(joinGame)
@@ -81,7 +71,7 @@ function joinGame(){
    
     if(name){
         $("#start-button").off('click');
-        $("#nickname #enter").off('click')
+        $("#nickname #enter").off('click');
         $(document).off("keypress");
         if(game.preload_complete){
             login();
